15211787d30d28b98e44390345aa135e6efabad0
Larry Markley <lmarkley12@gmail.com>
2018-07-16 21:53:15 -0500

    added 'did' alias

--- shell_files/aliases.sh
+++ shell_files/aliases.sh
@@ -7,3 +7,4 @@ alias afk='m lock'
 alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias ccat='ccat -G Keyword="darkgreen" -G Type="faint"'
 alias spoofMAC="ifconfig en0 ether $(openssl rand -hex 6 | sed 's%\(..\)%\1:%g; s%.$%%')"
+alias did="vim +'normal Go' +'r!date' ~/did.txt"
